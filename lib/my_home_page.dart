bbb9862e8df3c6376df8ff3ef41dc5e2f441b0a2
Rithik Bhandari <rithikbhandari895@gmail.com>
2020-11-22 16:04:37 +0530

    added profile_picture.dart

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_common_widgets/widgets/profile_picture.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
@@ -10,7 +11,17 @@ class MyHomePage extends StatelessWidget {
         ),
         body: ListView(
           padding: EdgeInsets.all(10),
-          children: [],
+          children: [
+            _widgetShowcase(
+              title: "profile_picture.dart",
+              widget: ProfilePicture(
+                "https://images.unsplash.com/photo-1605964883324-6d18a190faf3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=631&q=80",
+                radius: 50,
+                borderWidth: 2,
+                borderColor: Colors.yellow,
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/profile_picture.dart
@@ -0,0 +1,49 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ProfilePicture extends StatelessWidget {
+  final String url;
+  final double radius;
+  final double borderWidth;
+  final Color borderColor;
+  final VoidCallback onPressed;
+  final VoidCallback onLongPress;
+
+  ProfilePicture(
+    this.url, {
+    this.radius = 50.0,
+    this.borderWidth = 0.0,
+    this.borderColor = Colors.black,
+    this.onPressed,
+    this.onLongPress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: this.onPressed,
+      onLongPress: this.onLongPress,
+      child: CircleAvatar(
+        backgroundColor: this.borderColor,
+        radius: this.radius + this.borderWidth,
+        child: Container(
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(shape: BoxShape.circle),
+          child: CachedNetworkImage(
+            fit: BoxFit.cover,
+            width: this.radius * 2,
+            height: this.radius * 2,
+            imageUrl: this.url,
+            placeholder: (context, url) => Shimmer.fromColors(
+              child: CircleAvatar(radius: this.radius),
+              baseColor: Colors.grey,
+              highlightColor: Colors.grey[300],
+            ),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
+        ),
+      ),
+    );
+  }
+}
